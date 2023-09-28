5824277aef7cf982fcaac1ebff0a10d5946f2479
ropa240 <135818760+ropa240@users.noreply.github.com>
2023-09-28 13:51:59 +0900

    Add files via upload

--- kadai_016/php-book-app/index.php
+++ kadai_016/php-book-app/index.php
@@ -5,7 +5,7 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>書籍管理アプリ</title>
-     <link rel="stylesheet" href="css/style.css">
+     <link rel="stylesheet" href=".//css/style.css">
  
      <!-- Google Fontsの読み込み -->
      <link rel="preconnect" href="https://fonts.googleapis.com">
